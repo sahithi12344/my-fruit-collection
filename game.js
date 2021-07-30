f0535b2ea3830838550ce22bf55823efeffa97ef
sahithi12344 <75563200+sahithi12344@users.noreply.github.com>
2021-07-30 20:36:51 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,154 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+   
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                   
+
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
+                       
+                     if(index === player.index){
+                         
+                       //add code to display the player's name on the respective basket.
+                       textSize(20)
+                       fill("black")
+                       textFont("romans time")
+                       text(allPlayers[plr].name,x-25,y+25);
+
+                       fill("aqua")
+                       textSize(30)
+                       textFont("freeStyle")
+                       text("player1Score :  " +score ,90,100);
+                       text("player2score :  " +score ,400,100);
+                       
+                 if(player1.isTouching(fruitGroup)){ 
+                     var scoreref = database.ref('score');
+                    scoreref.on("value", function (data) {
+                       scoreref = data.val();
+                     score = score+10;
+                    })
+                }
+
+                if(player2.isTouching(fruitGroup)){
+                    var score1ref = database.ref('score1');
+                    score1ref.on("value", function (data) {
+                        score1ref = data.val(); 
+                 score1 = score1+1;
+             })
+
+            }
+                     }
+                    
+                      
+                 
+                 }
+                
+                
+                 
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+                 
+                  if (player.index !== null) {
+                    for (var i = 0; i < fruitGroup.length; i++) {
+                        if (fruitGroup.get(i).isTouching(players)) {
+                            fruitGroup.get(i).destroy();
+                         
+                            
+                        }
+
+                        
+                     
+
+                        
+                    }
+                  }
+                
+
+         
+         
+        
+         
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
